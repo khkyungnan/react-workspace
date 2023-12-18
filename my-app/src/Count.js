7819cdb3c0dac6599a5000b16d1634d13e04c66f
khkyungnan <143380461+khkyungnan@users.noreply.github.com>
2023-12-18 14:15:44 +0900

    FF

---
+++ my-app/src/Count.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+
+//증가시키는 함수
+function addCount (){
+
+}
+
+function Count(){
+
+    return (
+        <div>
+            <addCount />
+        </div>
+    )
+
+}
+
+export default Count;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Count from './Count';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/*<App />  react에서 주석은 {/* * /} 표기함   */} 
+    <Count />
   </React.StrictMode>
 );
 reportWebVitals();
