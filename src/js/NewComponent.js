b0897ef5a2b448b3abb83e3b3be89cea4af114bf
khkyungnan <143380461+khkyungnan@users.noreply.github.com>
2023-12-20 17:07:45 +0900

    FF

--- src/js/NewComponent.js
+++
@@ -1,8 +0,0 @@
-//NewComponent.js
-import React from "react";
-function R003_ImportComponent()  {
-                    return (
-                        <h2>hi</h2>
-                    )
-                }
-export default R003_ImportComponent;
