eacceb3d9bf16b80340b06464954ebb1e6d243ec
khkyungnan <143380461+khkyungnan@users.noreply.github.com>
2023-12-21 14:39:58 +0900

    FF

--- axios-props/src/App.js
+++ axios-props/src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import MovieList from "./MovieList";
+import DogImage from "./DogInfo";
 function App() {
   return (
     <div className="App">
-      <MovieList />
+      <DogImage />
     </div>
   );
 }
