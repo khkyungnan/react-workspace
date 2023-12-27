ade4969319fa7e491099fd75c60335b5a173f51a
khkyungnan <143380461+khkyungnan@users.noreply.github.com>
2023-12-27 14:06:09 +0900

    FF

--- Final-react-website/client/src/App.js
+++ Final-react-website/client/src/App.js
@@ -1,31 +1,32 @@
-import './App.css';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './Component/Header';
 import Footer from './Component/Footer';
 import Home from './Component/Home';
+import Header from './Component/Header';
+import MovieList from './Component/Movie/MovieList';
 import NumberGuessingGame from './Component/Game/NumberGuessingGame';
-
-//MovieList
-//TodoList
-//Quiz
-//NumberGuessingGame
-//Blog
+import Quiz from './Component/Game/Quiz';
+import TodoList from './Component/Todo/TodoList';
 
 function App() {
   return (
     <Router>
       <div>
         <Header />
+
         <div className="container mt-4">
           <Routes>
             <Route path="/" element={<Home />} />
-
+            <Route path="/movie" element={<MovieList />} />
+            <Route path="/todos" element={<TodoList />} />
             <Route
               path="/numberGuessingGame"
               element={<NumberGuessingGame />}
             />
+            <Route path="/quiz" element={<Quiz />} />
           </Routes>
         </div>
+
         <Footer />
       </div>
     </Router>
