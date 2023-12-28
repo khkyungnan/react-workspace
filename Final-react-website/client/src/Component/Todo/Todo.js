68c1d5dbf34c889bb73d7dcf456ac29b02ac601f
khkyungnan <143380461+khkyungnan@users.noreply.github.com>
2023-12-28 09:49:47 +0900

    FF

--- Final-react-website/client/src/Component/Todo/Todo.js
+++ Final-react-website/client/src/Component/Todo/Todo.js
@@ -1,9 +1,23 @@
 //Todo.js
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import CreatePags from './CreatePags';
+import ListPage from './ListPage';
 function Todo() {
   //js로 state 상태관리
-  const [action, setAction] = useState([]);
+
+  const Home = () => <div>Home</div>;
+  const [actions, setActions] = useState([]);
+
+  //생성하기
+  const addAction = (newAction) => {
+    setActions([...actions, newAction]);
+  };
+  //삭제하기
+  const deleteAction = (id) => {
+    setActions(actions.filter((action) => action.id !== id));
+  };
+
   return (
     <Router>
       <div>
@@ -18,8 +32,13 @@ function Todo() {
           </ul>
         </nav>
         <Routes>
-          <Route>메인</Route>
-          <Route>생성</Route>
+          <Route
+            path="/ "
+            element={<ListPage actions={actions} deleteAction={deleteAction} />}
+          />
+          <Route path="/create" element={<CreatePags action={addAction} />}>
+            생성
+          </Route>
         </Routes>
       </div>
     </Router>
